f2b402e8210f000d557518647f56f6b9938bd8da
sanjay kumar <sanjayasd45@gmail.com>
2024-11-15 15:14:22 +0530

    ASDf

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -9,7 +9,7 @@ router.get("/login/faild", (req, res) => {
     })
 })
 router.get("/login/success", (req, res) => {
-    console.log(req);
+    console.log("req.user => ",req.user);
     if(req.user){
         res.status(200).json({
             error : false,
